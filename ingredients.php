08db656efa445a37c81a8841a43aec1307f8677a
Philip Jake <philip@brembeck.me>
2022-02-02 00:34:42 +0100

    Update ingredients.php

--- ingredients.php
+++ ingredients.php
@@ -1,13 +1,17 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$json = file_get_contents('php://input');
-if(empty($json)){
+$textinput = file_get_contents('php://input');
+$param = $_GET['ingredients'];
+if(empty($textinput) && empty($param)){
 	http_response_code(400);
     echo '{"code":"bad_request","status":"400","message":"Missing parameter: text-input"}';
     die();
 }
+elseif(empty($param)){
+	$param = $textinput;
+}
 include_once ('src/isvegan.php');
-$response = explode(', ', $json);
+$response = explode(', ', $param);
 $result = array_uintersect($isvegan, $response, fn($a, $b) => strcasecmp($a, $b));
 if(empty($result)){
 	http_response_code(200);
